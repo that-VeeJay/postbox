a460c066f14e443e37877d577f83afea70584981
Vee Jay <veejay.programming@gmail.com>
2025-05-14 16:22:08 +0800

    chore: initial frontend setup
    - Configured Tailwind CSS and custom fonts
    
    - Integrated ShadCN UI components
    
    - Enabled dark mode support

---
+++ web/src/App.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from "./components/mode-toggle";
+
+export default function App() {
+  return (
+    <>
+      <ModeToggle />
+      <div>APP COMPONENT</div>
+    </>
+  );
+}
