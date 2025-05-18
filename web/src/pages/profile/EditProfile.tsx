dcaac4dd34461f9404f169cc3648b8fa3af0d481
Vee Jay <veejay.programming@gmail.com>
2025-05-18 14:43:05 +0800

    fix: resolve potential hydration error in component rendering

--- web/src/pages/profile/EditProfile.tsx
+++ web/src/pages/profile/EditProfile.tsx
@@ -47,7 +47,7 @@ export default function EditProfile({
 
   return (
     <Sheet>
-      <SheetTrigger>
+      <SheetTrigger asChild>
         <Button variant="outline">Edit Profile</Button>
       </SheetTrigger>
       <SheetContent>
