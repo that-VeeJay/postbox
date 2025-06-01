8519731d90681e19f699371d14951f3923f9ffd6
Vee Jay <veejay.programming@gmail.com>
2025-06-02 00:52:18 +0800

    chore: add barryvdh/laravel-ide-helper as dev dependency

---
+++ api/_ide_helper_models.php
@@ -0,0 +1,118 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $post_id
+ * @property int $user_id
+ * @property string $body
+ * @property int|null $parent_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, Comment> $replies
+ * @property-read int|null $replies_count
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereParentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment wherePostId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Comment whereUserId($value)
+ */
+	class Comment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $category
+ * @property string $title
+ * @property string $slug
+ * @property string $body
+ * @property string $image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $likes
+ * @property-read int|null $likes_count
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\PostFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereCategory($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Post whereUserId($value)
+ */
+	class Post extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $username
+ * @property string|null $location
+ * @property string|null $bio
+ * @property string|null $profile_picture
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Post> $likes
+ * @property-read int|null $likes_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Post> $posts
+ * @property-read int|null $posts_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereBio($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereProfilePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUsername($value)
+ */
+	class User extends \Eloquent {}
+}
+
--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -22,46 +22,74 @@ class PostController extends Controller
             'recent' => $this->recentPosts(),
             'latest' => $this->latestPosts(),
             'featured' => $this->featuredPost(),
-            default => response()->json(Post::with('user')->latest()->paginate(self::PAGINATE)),
+            default => response()
+                        ->json(Post::with('user')
+                        ->latest()
+                        ->paginate(self::PAGINATE)
+            ),
         };
     }
 
     public function featuredPost()
     {
-        $featuredPost = Post::with('user')->inRandomOrder()->first();
+        $featuredPost = Post::with('user')
+            ->inRandomOrder()
+            ->first();
+
         return response()->json($featuredPost);
     }
 
     public function recentPosts(): JsonResponse
     {
-        $recentPosts = Post::with('user')->latest()->take(self::RECENT_POSTS)->get();
+        $recentPosts = Post::with('user')
+            ->latest()
+            ->take(self::RECENT_POSTS)
+            ->get();
+
         return response()->json($recentPosts);
     }
 
     public function latestPosts(): JsonResponse
     {
-        $latestPosts = Post::with('user')->latest()->skip(self::SKIP_POSTS)->take(self::LATEST_POSTS)->get();
+        $latestPosts = Post::with('user')
+            ->latest()
+            ->skip(self::SKIP_POSTS)
+            ->take(self::LATEST_POSTS)
+            ->get();
+
         return response()->json($latestPosts);
     }
 
     public function userPosts(int $id):JsonResponse
     {
-        $posts = Post::where('user_id', $id)->select('title', 'image')->latest()->get();
+        $posts = Post::where('user_id', $id)
+            ->select('title', 'image')
+            ->latest()
+            ->get();
+
         return response()->json($posts);
     }
 
     public function show(string $slug): JsonResponse
     {
         $user = Auth::user();
-        $post = Post::where('slug', $slug)->withCount('likes')->firstOrFail();
+        $post = Post::where('slug', $slug)
+            ->withCount('likes')
+            ->firstOrFail();
 
         $liked = false;
 
         if($user) {
-            $liked = $user->likes()->where('post_id', $post->id)->exists();
+            $liked = $user->likes()
+                ->where('post_id', $post->id)
+                ->exists();
         }
 
-        return response()->json(['post' => $post, 'user' => $post->user, 'liked_by_current_user' => $liked]);
+        return response()->json([
+            'post' => $post,
+            'user' => $post->user,
+            'liked_by_current_user' => $liked
+        ]);
     }
 
     public function destroy(string $slug): JsonResponse
@@ -69,7 +97,9 @@ class PostController extends Controller
          $post = Post::where('slug', $slug)->first();
          $post->delete();
 
-         return response()->json(['message' => 'The posted was deleted.']);
+         return response()->json([
+            'message' => 'The posted was deleted.'
+        ]);
     }
 
     public function store(Request $request, ImageService $imageService, SlugService $slug): JsonResponse
@@ -88,7 +118,10 @@ class PostController extends Controller
 
         $validated['slug'] = $slug->generateUniqueSlug($validated['title']);
 
-        $post = $request->user()->posts()->create($validated);
+        $post = $request
+            ->user()
+            ->posts()
+            ->create($validated);
 
         return response()->json(['post' => $post, 'user' => $post->user]);
     }
